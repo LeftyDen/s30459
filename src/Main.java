d29048b7cda9d536c283bf24b079330fc8e0e335
YuriiDombrovsky <s29915@pjwstk.edu.pl>
2024-10-08 13:29:17 +0200

    norm

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 // TODO: we need to add the missing classes!
 
-
 // Ok, I will 'Adder' and s29915 will add 'Subtracor'.
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(1, 5));
 
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
