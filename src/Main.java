3d508b45930d4c418c5690b130d479b2792fafb1
LeftyDen <s30459@pjwstk.edu.pl>
2024-10-08 12:37:03 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adeer();
+        System.out.println(adder.add(1, 2));
+
+        Subtractor subtractor = new Subtractor();
+        System.out.println(subtractor.subtract(6, 3));
+    }
+}
